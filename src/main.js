857f9417b3c9085e22c4f9183a034ee5a2377f81
RomanovRoma <romanian.reget@gmail.com>
2021-08-19 01:17:32 +0300

    add @babel/preset-react

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
+import React from "react"
+import ReactDOM from 'react-dom';
 
-class App {
-  run = async (name = "World") => {
-    console.log(`Hello ${name}`);
-  };
-}
+const App = () => <p>Hello</p>
 
-const app = new App();
-app
-  .run()
-  .then(() => console.log("done"))
-  .catch(() => console.log("Error!"));
+ReactDOM.render(<App/>,
+  document.getElementById('root'))
